85a3d2dc0ab492ebcb36467689aaa2263d73f6e9
Mark R Baker <mrbaker1917@gmail.com>
2020-09-28 15:56:04 -0700

    adds slyles to DayItemList component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,15 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
   return (
-    <li onClick={() => props.setDay(props.setDay)}>
+    <li className={dayClass} onClick={() => props.setDay(props.setDay)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
     </li>
