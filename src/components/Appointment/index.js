46adb42560acd413ac1ca49c480e306089b8460d
Mark R Baker <mrbaker1917@gmail.com>
2020-09-29 16:55:45 -0700

    adds Header and Empty components to Appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import "./styles.scss";
+import Header from "./Header";
+import Empty from "./Empty";
 
 function Appointment(props) {
   return (
     <article className="appointment">
-
+      <Header time="12pm" />
+      {/* <Empty addOn={props.addOn} /> */}
     </article>
   );
 }
