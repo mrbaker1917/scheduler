f6aa9e64e5e7d97537fcbc855d27981f6fe7eb61
Mark R Baker <mrbaker1917@gmail.com>
2020-09-29 16:14:24 -0700

    sets up styles and stories for Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles.scss";
+
+function Appointment(props) {
+  return (
+    <article className="appointment">
+
+    </article>
+  );
+}
+
+export default Appointment;
