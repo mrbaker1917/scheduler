f66c0eaa5986495d6d59161867804d7c7e05977a
Mark R Baker <mrbaker1917@gmail.com>
2020-09-30 09:20:09 -0700

    fixes functionality in form component.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,28 +4,35 @@ import Button from "components/Button";
 
 const Form = (props) => {
   const [name, setName] = useState(props.name || "");
-const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  }
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
-            name={props.name}
+            value={name}
             type="text"
             placeholder="Enter Student Name"
             onChange={(event) => setName(event.target.value)}
-            onClick={() => setInterviewer(props.interviewer)}
           /*
             This must be a controlled component
           */
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={props.setInterviewer} />
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={props.onCancel} danger>Cancel</Button>
+          <Button danger onClick={() => cancel()} >Cancel</Button>
           <Button onClick={props.onSave} confirm>Save</Button>
         </section>
       </section>
