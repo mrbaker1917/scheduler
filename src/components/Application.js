672d8a9aa45ea17df313621e2443ee67567b5543
Mark R Baker <mrbaker1917@gmail.com>
2020-09-30 13:54:44 -0700

    updates Application component with mock data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,8 @@ import DayList from "./DayList";
 
 import "components/Application.scss";
 import InterviewerList from "./InterviewerList";
+import Appointment from "components/Appointment";
+
 const days = [
   {
     id: 1,
@@ -28,6 +30,52 @@ const interviewers = [
   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
 ];
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "2pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "Jasper Baker-McCue",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "4pm",
+    interview: {
+      student: "Lily Baker-McCue",
+      interviewer: {
+        id: 1,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "5pm",
+  }
+];
 
 export default function Application() {
   const [day, setDay] = useState("Monday");
@@ -42,16 +90,19 @@ export default function Application() {
             days={days}
             day={day}
             setDay={setDay}
-            />
+          />
         </nav>
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
       <section className="schedule">
-        <InterviewerList
-          interviewers={interviewers}
-          interviewer={interviewer}
-          setInterviewer={setInterviewer}
-        />
+        <ul>
+          {appointments.map(appointment => {
+            return (
+              <Appointment key={appointment.id} {...appointment} />
+            );
+          })}
+          <Appointment key="last" time="5pm" />
+        </ul>
       </section>
     </main>
   );
