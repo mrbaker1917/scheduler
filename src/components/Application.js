4f13ce9a95d404de7e71edd739e834f3a2df098c
Mark R Baker <mrbaker1917@gmail.com>
2020-10-01 08:42:07 -0700

    combines states into one state object and adapts app.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -61,9 +61,13 @@ const appointments = [
 ];
 
 export default function Application() {
-  const [day, setDay] = useState("Monday");
-  const [interviewer, setInterviewer] = useState("Sylvia Palmer");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday", 
+    days: [],
+    appointments: {}
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
   const baseURL = "http://192.168.1.69:8001";
 
   useEffect(() => {
@@ -73,6 +77,7 @@ export default function Application() {
         setDays(daysApi)
       });
   }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -80,8 +85,8 @@ export default function Application() {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
