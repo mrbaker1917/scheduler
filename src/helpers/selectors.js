7e7586d05808162a3bcb419cfec943c37b0a79c9
Mark R Baker <mrbaker1917@gmail.com>
2020-10-02 10:10:33 -0700

    adds getInterviewersByDay and tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -10,9 +10,26 @@ export function getAppointmentsForDay(state, day) {
   };
   let daysApptsDets = [];
   for (let appt of daysApptIDs) {
-    daysApptsDets.push(state.appointments[appt])
+    daysApptsDets.push(state.appointments[appt]);
   };
   return daysApptsDets;
+};
+
+export function getInterviewersForDay(state, day) {
+  let daysInterviewerIDs = [];
+  for (let d of state.days) {
+    if (d.name === day) {
+      daysInterviewerIDs = d.interviewers;
+    }
+  };
+  if (daysInterviewerIDs.length === 0) {
+    return [];
+  };
+  let daysInterviewerDets = [];
+  for (let itv of daysInterviewerIDs) {
+    daysInterviewerDets.push(state.interviewers[itv]);
+  };
+  return daysInterviewerDets;
 };
 
 export function getInterview(state, interview) {
