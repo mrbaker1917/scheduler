aea2550f7e1235a7b4bd0c76d738f1f3e5bcc33d
Mark R Baker <mrbaker1917@gmail.com>
2020-10-01 19:19:19 -0700

    adds useVisualMode custom hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (replace === true) {
+      history.pop();
+      setMode(newMode);
+    } else {
+      setMode(newMode);
+      setHistory(history.concat([newMode]));
+    }
+  }
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    } else {
+      setMode(history[0]);
+    }
+
+  }
+
+  return { mode, transition, back };
+};
