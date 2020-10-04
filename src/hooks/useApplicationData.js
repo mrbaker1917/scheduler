22d96e77b9d605c9c40be32bfd1e971d959188fa
mrbaker1917 <mrbaker1917@gmail.com>
2020-10-04 16:48:54 -0700

    changes useApplicationData to useReducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,68 +1,90 @@
-import { useState, useEffect } from "react";
+import { useReducer, useEffect } from "react";
 import axios from "axios";
 
 export function useApplicationData(initial) {
-  const [state, setState] = useState({
+  const SET_DAY = "SET_DAY";
+  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+  const SET_INTERVIEW = "SET_INTERVIEW";
+
+  const [state, dispatch] = useReducer( reducer, {
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: [],
   });
-  const setDay = (day) => setState({ ...state, day });
 
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
+  const setDay = (day) => dispatch({ type: SET_DAY, day });
 
+  function reducer(state, action) {
+    switch (action.type) {
+      case SET_DAY: 
+        return {
+          ...state,
+          day: action.day
+        };
+      
+      case SET_APPLICATION_DATA: 
+        return {
+          ...state,
+          days: action.days,
+          appointments: action.appointments,
+          interviewers: action.interviewers,
+        };
+        
+      case SET_INTERVIEW:
+        {
+          const appointment = {
+            ...state.appointments[action.appointment_id],
+            interview: action.interview && { ...action.interview },
+          };
+          const appointments = {
+            ...state.appointments,
+            [action.appointment_id]: appointment,
+          };
+          return {
+            ...state,
+            appointments
+          }
+        }
+      default:
+        throw new Error(
+          `Tried to reduce with unsupported action type: ${action.type}`
+        );
+    }
+  }
+
+  function bookInterview(appointment_id, interview) {
     const newDays = state.days.map((day) => {
-      if (day.appointments.includes(id)) {
+      if (day.appointments.includes(appointment_id)) {
         day.spots = day.spots - 1;
       }
       return day;
     });
 
     return axios
-      .put(`/api/appointments/${id}`, { interview })
+      .put(`/api/appointments/${appointment_id}`, { interview })
       .then((response) => {
         if (response.status === 204) {
-          setState({
-            ...state,
-            appointments,
-            newDays,
+          dispatch({ type: SET_INTERVIEW,
+            appointment_id, interview, newDays
           });
         }
       });
   }
 
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null,
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
+  function cancelInterview(appointment_id) {
 
     const newDays = state.days.map((day) => {
-      if (day.appointments.includes(id)) {
+      if (day.appointments.includes(appointment_id)) {
         day.spots = day.spots + 1;
       }
       return day;
     });
 
-    return axios.delete(`/api/appointments/${id}`).then((response) => {
+    return axios.delete(`/api/appointments/${appointment_id}`).then((response) => {
       if (response.status === 204) {
-        setState({
-          ...state,
-          appointments,
-          newDays,
+        dispatch({ type: SET_INTERVIEW,
+          appointment_id, interview: null, newDays
         });
       }
     });
@@ -75,12 +97,12 @@ export function useApplicationData(initial) {
     const interviewers = axios.get(`${baseURL}/api/interviewers`);
     const promises = [days, appointments, interviewers];
     Promise.all(promises).then((arrOfResponses) => {
-      setState((prev) => ({
-        ...prev,
+      dispatch({
+        type: SET_APPLICATION_DATA,
         days: arrOfResponses[0].data,
         appointments: arrOfResponses[1].data,
         interviewers: arrOfResponses[2].data,
-      }));
+      });
     });
   }, []);
 
